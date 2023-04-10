7cac798627a81dc48f053a254b2943c176278b17
Eren-Enou <erenenou@yahoo.com>
2023-04-10 07:30:23 -0700

    finished post card, createpost, home, single post'

--- blog/src/components/PostCard.jsx
+++ blog/src/components/PostCard.jsx
@@ -5,15 +5,14 @@ export default function PostCard({ post }) {
     return (
         <div className="card mt-3">
             <div className="row g-0">
-                <div className="col-md-4">
-                    <img className="card-img-top" src={post.image_url} alt="random" />
+                <div className="col-12 col-md-6 col-lg-3">
+                    <img className="card-img-top" src="https://picsum.photos/115" alt="random" />
                 </div>
                 <div className="col-md-8">
                     <div className="card-body">
                         <h6 className="card-subtitle text-muted">{ post.date_created }</h6>
                         <h5 className="card-title">{ post.title }</h5>
-                        <h6 className="card-subtitle">By: { post.user_id }</h6>
-                        <p className="card-text">{ post.body }</p>
+                        <p className="card-text">{ post.content }</p>
                         <Link className='btn btn-primary' to={`/posts/${post.id}`}>See More</Link>
                     </div>
                 </div>
--- blog/src/views/SinglePost.jsx
+++ blog/src/views/SinglePost.jsx
@@ -1,24 +1,110 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import PostCard from '../components/PostCard';
 
 export default function SinglePost() {
     const params = useParams();
+    const navigate =  useNavigate();
 
     const [post, setPost] = useState({});
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
 
     useEffect(() => {
-        fetch(`http://localhost:5000/api/posts/${params.postId}`)
+        fetch(`https://kekambas-blog-api.onrender.com/${params.postId}`)
             .then(res => res.json())
             .then(data => {
                 console.log(data);
                 setPost(data);
+                setTitle(data.title);
+                setContent(data.content);
             })
     }, [params.postId])
     
-    return (
-        <div>
-            <PostCard post={post} />
-        </div>
-    )
-}
+	async function handleSubmit(e) {
+		e.preventDefault();
+
+		// Get the updated data from the form
+		let updatedTitle = title;
+		let updatedContent = content;
+
+		// Get the JWT token from localStorage
+		let token = localStorage.getItem("token");
+
+		// Set up the request headers
+		let myHeaders = new Headers();
+		myHeaders.append("Content-Type", "application/json");
+		myHeaders.append("Authorization", `Bearer ${token}`);
+
+		// Set up the request body
+		let requestBody = JSON.stringify({
+			title: updatedTitle,
+			content: updatedContent,
+		});
+
+		// Make the fetch request
+		let response = await fetch(
+			`https://kekambas-blog-api.onrender.com/api/posts/${params.postId}`,
+			{
+				method: "PUT",
+				headers: myHeaders,
+				body: requestBody,
+			}
+		);
+
+		let data = await response.json();
+
+		if (data.error) {
+			console.log(data.error, "danger");
+			navigate("/login");
+		} else {
+			console.log(`${data.title} has been updated`, "success");
+			navigate("/");
+		}
+	}
+
+     async function handleDelete(e) {
+				e.preventDefault();
+
+				// Get the JWT token from localStorage
+				let token = localStorage.getItem("token");
+
+				// Set up the request headers
+				let myHeaders = new Headers();
+				myHeaders.append("Authorization", `Bearer ${token}`);
+
+				// Make the fetch request
+				let response = await fetch(
+					`https://kekambas-blog-api.onrender.com/api/posts/${params.postId}`,
+					{
+						method: "DELETE",
+						headers: myHeaders,
+					}
+				);
+
+				let data = await response.json();
+
+				if (data.error) {
+					console.log(data.error, " error");
+				} else {
+					console.log(`${data.success}`, " success");
+					navigate("/");
+				}
+			}
+    
+	return (
+		<>
+			<h2 className="text-center">Post Options</h2>
+			<form onSubmit={handleSubmit}>
+				<div className="form-group">
+					<input type="text" name="title" className="form-control my-3" placeholder="Enter Title" value={title} onChange={(e) => setTitle(e.target.value)}/>
+					<textarea name="content" className="form-control my-3" placeholder="Enter Body" value={content} onChange={(e) => setContent(e.target.value)}/>
+                    <div className="btn-group text-center">
+                        <input type="submit" value="Update Post" className="text-center btn btn-success w-50"/>
+                        <button className="btn btn-danger w-50" onClick={handleDelete}>Delete Post</button>
+				    </div>
+                </div>
+			</form>
+		</>
+	);
+};
